79775301b379ee08e48cc072b50673c223fa2057
Sam Portnow <samson91787@gmail.com>
2013-02-20 22:14:27 -0500

    Adding Files

--- test_git.py
+++ test_git.py
@@ -48,16 +48,50 @@ def add(path, file):
     shell = False
     ).communicate()
 
+def get_hash_commit(path):
+    try:
+        os.mkdir(path)
+    except:
+        pass
+
+    return subprocess.Popen(
+        ["git", "rev-parse", "HEAD"],
+        cwd = path,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        shell = False
+    ).communicate()
+
+def get_hash_file(path, filename):
+    try:
+        os.mkdir(path)
+    except:
+        pass
+    file_and_hash = {}
+    file = subprocess.Popen(
+    ["git", "ls-files", "-s", filename],
+    cwd = path,
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE,
+    shell = False
+    ).communicate()
+    position = file[0].find(filename)
+    hash = file[0][:position].strip()
+    file = file[0][position:].strip()
+    file_and_hash[file] = hash
+    return file_and_hash
+
 #https://help.github.com/articles/remove-sensitive-data
 
 #init("/Users/samportnow/Documents/Git")
-add("/Users/samportnow/Documents/Git",".")
-commit("/Users/samportnow/Documents/Git", "Sam Portnow", "samson91787@gmail.com", "Made some changes")
+#add("/Users/samportnow/Documents/Git",".")
+#commit("/Users/samportnow/Documents/Git", "Sam Portnow", "samson91787@gmail.com", "Made some changes")
+print get_hash_commit("/Users/samportnow/Documents/Git")
+print get_hash_file("/Users/samportnow/Documents/Git","test_git.py")
 # def commit
 # def init
 # def add
 
 # def get_file ... this will be a special function
 # def file_delete ... this will be a special function
-
 #def file_versions
